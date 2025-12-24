0a780b49ae8ee38181986ccec641f15f80754c7c
Attiq <attiqbashir0@gmail.com>
2025-12-24 11:22:46 +0000

    initial setup

---
+++ src/test/java/com/a99ti/books/BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a99ti.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
