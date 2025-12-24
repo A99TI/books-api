1f27b9562de8b66c12bcd1fc4c5421ead9a1fda4
Attiq <attiqbashir0@gmail.com>
2025-12-24 16:06:58 +0000

    implemented basic delete to /book route

--- src/main/java/com/a99ti/books/controller/BookController.java
+++ src/main/java/com/a99ti/books/controller/BookController.java
@@ -62,4 +62,9 @@ public class BookController {
 
     }
 
+    @DeleteMapping("/api/books/{title}")
+    public void deleteBook(@PathVariable String title){
+        books.removeIf(book -> book.getTitle().equalsIgnoreCase(title));
+    }
+
 }
