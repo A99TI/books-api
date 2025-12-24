083500c60b3881ca38212e086ad98883f6adbecc
Attiq <attiqbashir0@gmail.com>
2025-12-24 12:07:25 +0000

    created hard coded books array + created basic get book endpoint

--- src/main/java/com/a99ti/books/controller/BookController.java
+++ src/main/java/com/a99ti/books/controller/BookController.java
@@ -1,14 +1,34 @@
 package com.a99ti.books.controller;
 
+import com.a99ti.books.entities.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class BookController {
 
-    @GetMapping("/api")
-    public String firstAPI(){
-        return "Hello World";
+    private final List<Book> books = new ArrayList<>();
+
+    public BookController (){
+        initialiseBooks();
+    }
+
+    private void initialiseBooks() {
+        books.addAll(List.of(
+                new Book("The Pragmatic Programmer", "Andrew Hunt & David Thomas", "Software Development"),
+                new Book("Clean Code", "Robert C. Martin", "Programming"),
+                new Book("Effective Java", "Joshua Bloch", "Java"),
+                new Book("Design Patterns", "Erich Gamma et al.", "Software Engineering"),
+                new Book("Introduction to Algorithms", "Thomas H. Cormen", "Computer Science")
+        ));
+    }
+
+    @GetMapping("/api/v1/books")
+    public List<Book> getBooks(){
+        return books;
     }
 
 }
